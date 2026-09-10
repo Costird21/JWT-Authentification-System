1b1f2c657a1c3b8080dea3d951b3de668ade5c2b
Daniel Radu <danielradu2107@gmail.com>
2026-09-10 20:05:21 +0300

    implemented getSigningKey() method

--- src/main/java/com/danielradu/security/config/JwtService.java
+++ src/main/java/com/danielradu/security/config/JwtService.java
@@ -1,14 +1,33 @@
 package com.danielradu.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "3d67719e7a9ee593f0f7bc508e9f557a4d27773b3a65ae95c281804f70be504d";
 
     public String extractUsername(String jwtToken) {
-
         return null;
+    }
 
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
