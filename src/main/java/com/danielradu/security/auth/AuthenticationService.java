d5885fc561a804b68fa0648fac3e6c092bc293cc
Daniel Radu <danielradu2107@gmail.com>
2026-09-10 21:27:09 +0300

    implemented the register service method

--- src/main/java/com/danielradu/security/auth/AuthenticationService.java
+++ src/main/java/com/danielradu/security/auth/AuthenticationService.java
@@ -1,16 +1,36 @@
 package com.danielradu.security.auth;
 
+import com.danielradu.security.config.JwtService;
+import com.danielradu.security.user.Role;
+import com.danielradu.security.user.User;
+import com.danielradu.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest registerRequest) {
 
-        return null;
+        var user = User.builder()
+                .first_name(registerRequest.getFirst_name())
+                .last_name(registerRequest.getLast_name())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
 
     }
 
