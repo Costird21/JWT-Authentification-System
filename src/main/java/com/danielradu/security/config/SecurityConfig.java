c4cc4df49301c1cf886b807c396f5b8158f13160
Daniel Radu <danielradu2107@gmail.com>
2026-09-10 21:36:17 +0300

    created a demo controller and allowed the register endpoint to be visible

--- src/main/java/com/danielradu/security/config/SecurityConfig.java
+++ src/main/java/com/danielradu/security/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         (authorize) -> authorize
-                                .requestMatchers("")
+                                .requestMatchers("/api/v1/auth/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
